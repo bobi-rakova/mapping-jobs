cb0bb0bb06aee99fdfda84402e9401f11d3b8794
Bobi Rakova <b.rakova@gmail.com>
2014-07-25 00:47:28 -0700

    Version #1

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,24 @@
-$.post( "/add-new", { 
-	name: $("#name").val(), 
-	email: $("#email").val(),
-	title: $("#title").val(),
-	description: $("#description").val(),
-	skillset: $("#skillset").val()
-} );
+$(function(){
+	function loadJosters() {
+		$.get("/get-all", function(res, err){
+			console.log(res)
+
+			var jobs = []
+			for(var i=0; i<res.length; i++)
+				jobs.push(JSON.parse(res[i]))
+			
+			var objects = {items: jobs}
+			var template = $('#template').html();
+			// Mustache.parse(template);
+			var rendered = Mustache.render(template, objects);
+			$('#target').html(rendered);
+		})
+
+	  //var template = $('#template').html();
+	  //Mustache.parse(template);   // optional, speeds up future uses
+	  //var rendered = Mustache.render(template, {name: "Luke"});
+	  //$('#target').html(rendered);
+	}
+
+	loadJosters()
+})
--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 var express = require('express');
 var app = express();
 
+var redis = require('redis')
+var client = redis.createClient(6379, "127.0.0.1")
+
+client.on('error', function(error){
+	console.log('redis error ' + error)
+})
+
+client.on('ready', function(){
+	console.log('Connected to redis')
+})
+
+client.on('end', function(){
+	console.log('End-ing redis')
+})
+
 var bodyParser = require('body-parser')
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded())
@@ -8,9 +23,17 @@ app.use(bodyParser.urlencoded())
 app.use(express.static(__dirname + '/public'));
 
 app.post('/add-new', function(req, res){
-	var data = req.body;
-	console.log('POST request received: ' + JSON.stringify(data));
+	var data = JSON.stringify(req.body);
+	// console.log('POST request received: ' + data);
+	client.lpush('jobs-list', data)
 	res.send(200);
+});
+
+app.get('/get-all', function(req, res){
+	var data = client.lrange('jobs-list', 0, 100, function(err, data) {
+		// console.log("GET: " + data)
+  		res.send(data);
+	})
 });
 
 app.listen(3000);
